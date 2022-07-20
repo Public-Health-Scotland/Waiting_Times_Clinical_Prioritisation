458ee79cac387f6794b764c51029e9c0e653c3db
RosalynLP <rosalyn.pearson@phs.scot>
2022-07-20 13:26:21 +0100

    load data from shiny_app/data

--- shiny_app/functions/core_functions.R
+++ shiny_app/functions/core_functions.R
@@ -1,5 +1,13 @@
 ####################### Core Functions #######################
 
+# Load data from shiny_app/data
+load_rds_file <- function(rds){
+  # Given a .rds file name in shiny_app/data
+  # this function loads it in to app_data list object
+  # NB you must create this object first using app_data <- list()
+  app_data[[gsub(".rds", "", rds)]] <<- readRDS(paste0("data/", rds))
+}
+
 # Remove warnings from icons ----
 icon_no_warning_fn = function(icon_name) {
   icon(icon_name, verify_fa=FALSE)
--- shiny_app/setup.R
+++ shiny_app/setup.R
@@ -18,3 +18,14 @@ library(phsstyles)
 # Load packages and functions, and data wrangling
 source("functions/core_functions.R")
 
+# Initialise list to store all app data
+app_data <- list()
+
+# Load in data to app_data
+# Find all rds files in shiny_app/data
+rds_files <- list.files(path="data/", pattern="*.rds")
+
+for (rds in rds_files){
+  load_rds_file(rds)
+}
+
