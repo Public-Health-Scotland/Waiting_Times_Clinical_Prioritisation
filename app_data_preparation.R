1bf4c195f7a89a75343e25f42e5f0cf25c57e758
RosalynLP <rosalyn.pearson@phs.scot>
2022-07-20 13:05:13 +0100

    app data prep

--- app_data_preparation.R
+++ app_data_preparation.R
@@ -8,15 +8,28 @@
 rm(list = ls())
 gc()
 
-###############################################.
-## Functions/Packages/filepaths/lookups ----
-###############################################.
-
 # Getting project directory to choose files from
 project_directory <- rstudioapi::getActiveProject()
 if (!is.null(project_directory)){ setwd(project_directory) }
 
 processed_data_folder <- "data/processed data/"
 shiny_data_folder <- "shiny_app/data/"
+
+# Load all processed data files and save out as rds
+
+copy_to_shiny_data <- function(csv){
+  # Given a .csv file name in processed_data_folder
+  # copies it across to an .rds file in shiny_data_folder
+  readfile <- read_csv(paste0(processed_data_folder, csv))
+  saveRDS(readfile, paste0(shiny_data_folder, gsub(".csv", ".rds", csv)))
+}
+
+# Find all csv files in processed_data
+processed_data_csvs <- list.files(path=processed_data_folder, pattern="*.csv")
+
+for(csv in processed_data_csvs){
+  copy_to_shiny_data(csv)
+}
+
 
 
