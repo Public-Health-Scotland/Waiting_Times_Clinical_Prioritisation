94718359cdc619e89e371203f570db206266f4ea
RosalynLP <rosalyn.pearson@phs.scot>
2022-08-03 15:58:55 +0100

    mini change to median modal

--- shiny_app/functions/modals.R
+++ shiny_app/functions/modals.R
@@ -4,10 +4,9 @@
 
 ## Modal to explain what median is
 median_modal <- modalDialog(
-  h5("What is the median?"),
+  h3("What is the median?"),
   p("The median is the value separating the higher half from the lower half of
-    a data sample, a population, or a probability distribution. For a data set, it may be thought
-    of as \"the middle\" value."),
+    a data sample. Here it may be thought of as \"the middle\" value."),
   size = "l",
   easyClose = TRUE, fade=TRUE, footer = modalButton("Close (Esc)")
 )
