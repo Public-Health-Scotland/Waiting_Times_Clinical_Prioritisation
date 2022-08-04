4e7651cdb408d782999b9722be326139ab9bbe21
RosalynLP <rosalyn.pearson@phs.scot>
2022-08-04 13:25:41 +0100

    set up dependent options

--- shiny_app/pages/data_download.R
+++ shiny_app/pages/data_download.R
@@ -35,6 +35,19 @@ output$download_ui <-  renderUI({
                                                options = pickerOptions(
                                                  showTick = TRUE,
                                                  liveSearch =TRUE)
+                                   ),
+                                   pickerInput("download_dataset",
+                                               "4. Choose dataset    ",
+                                               choices = c("Patients waiting, admitted and seen",
+                                                           "Distribution of waits",
+                                                           "Activity",
+                                                           "Summary of patients waiting and admitted"),
+                                               selected = "Patients waiting, admitted and seen",
+                                               inline = TRUE,
+                                               multiple = FALSE,
+                                               width = "100%",
+                                               options = pickerOptions(
+                                                 showTick = TRUE)
                                    )
 
                                  )
@@ -44,4 +57,34 @@ output$download_ui <-  renderUI({
   ) # div
 
 }) # renderUI
+
+
+data_choices <- list("monthly" = c("Patients waiting, admitted and seen",
+                                   "Distribution of waits",
+                                   "Summary of patients waiting and admitted"),
+                     "quarterly" = c("Patients waiting, admitted and seen",
+                                     "Distribution of waits",
+                                     "Activity",
+                                     "Summary of patients waiting and admitted"))
+
+# This makes sure that choice of datasets update depending on whether
+# monthly or quarterly is selected
+observeEvent(
+
+  eventExpr=input$download_timescale,
+
+  handlerExpr={
+
+    if( !is.null(input$download_timescale) ) {
+
+      updatePickerInput(session, inputId="download_dataset",
+                        selected = "Patients waiting, admitted and seen",
+                        choices = data_choices[[input$download_timescale]]
+      )
+
+    }
+
+  }
+)
+
 
