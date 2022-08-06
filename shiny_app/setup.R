1fcbd42d795acbf4a92ea473d9af2a3b0a359be1
Caroline Thomson <caroline.thomson6@nhs.net>
2022-08-06 17:14:39 +0100

    Modified graph colours to match report (phs-graphite)

--- shiny_app/setup.R
+++ shiny_app/setup.R
@@ -44,7 +44,7 @@ xaxis_plots <- list(title = FALSE, tickfont = list(size=14), titlefont = list(si
 yaxis_plots <- list(title = FALSE, rangemode="tozero", fixedrange=TRUE, size = 4,
                     tickfont = list(size=14), titlefont = list(size=14))
 
-waiting_times_palette <- phs_colours(c("phs-green","phs-purple", "phs-blue", "phs-magenta", "phs-liberty"))
+waiting_times_palette <- phs_colours(c("phs-green","phs-purple", "phs-blue", "phs-magenta", "phs-graphite"))
 
 # Buttons to remove from plotly plots
 bttn_remove <-  list('select2d', 'lasso2d', 'zoomIn2d', 'zoomOut2d',
