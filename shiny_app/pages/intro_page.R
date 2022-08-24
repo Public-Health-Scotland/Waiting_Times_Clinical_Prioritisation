3f98129c02024401d471f1ffe8f4d4c83d00eab4
RosalynLP <rosalyn.pearson@phs.scot>
2022-08-24 10:51:40 +0100

    populating info page

--- shiny_app/pages/intro_page.R
+++ shiny_app/pages/intro_page.R
@@ -110,7 +110,25 @@ output$data_quality_notes <- renderUI({
 output$using_dashboard_notes <- renderUI({
 
   tagList(
-    h4("Using the dashboard")
+    h4("Using the dashboard"),
+    tags$li("There are tabs across the top of the dashboard for each topic area, and also buttons at the bottom
+      of this page to help you navigate around the dashboard."),
+    tags$li("You can find an interactive overview of the data on the 'Overview of waiting list activity' tab, and more in-depth
+            breakdowns by specialty and NHS Health Board of Treatment on the corresponding additional tabs."),
+    tags$li("If you would like to download data, please do this using the 'Download data' tab, which will
+            allow you to select custom data for download."),
+    br(),
+    h4("Interacting with the dashboard"),
+    tags$li("On each tab there are dropdown boxes which allow the user to filter the data they wish to
+            explore, for example by NHS Health Board of Treatment."),
+    tags$li("Move the mouse over the charts to see information about the underlying data"),
+    tags$li("Clicking on a category in the legend removes it from the chart, and double clicking will isolate
+            only that category. This is useful to reduce the number of lines on the chart, making them
+            easier to see. Clicking again on a removed category will add it back into the chart."),
+    tags$li("You can download an image of a chart in a given state by clicking the camera icon in the
+            top right hand corner of the chart."),
+    tags$li("Some panels have tabs on the upper right hand side which allow you to toggle between different
+            views, such as a graph and data view, or two different graph views.")
 
   )
 }) # render UI close bracket
@@ -118,7 +136,11 @@ output$using_dashboard_notes <- renderUI({
 output$further_info_notes <- renderUI({
 
   tagList(
-    h4("Further information")#,
+    h4("Further information"),
+    tags$li("The code used to produce this tool will soon be published on",
+       tags$a(href="https://github.com/Public-Health-Scotland", "GitHub (external website)")),
+    tags$li("Please contact the PHS Waiting Times team with any questions or comments on",
+            tags$a(href="mailto:phs.waitingtimes@phs.scot", "phs.waitingtimes@phs.scot"))
 
     #bsButton("jump_to_specialties", label = "Go to specialties")
   )
@@ -128,7 +150,37 @@ output$further_info_notes <- renderUI({
 output$accessibility_notes <- renderUI({
 
   tagList(
-    h4("Accessibility")#,
+    h4("Accessibility"),
+    tags$li("This website is run by", tags$a(href="https://www.publichealthscotland.scot/",
+                                             "Public Health Scotland,"), "Scotland's national
+            organisation for public health. As a new organisation formed on 1 April 2020,
+            Public Health Scotland is currently reviewing its web estate. Public Health Scotland
+            is committed to making its website accessible, in accordance with the Public Sector
+            Bodies (Websites and Mobile Applications)(No. 2) Accessibility Regulations 2018.
+            This accessibility statement applies to this dashboard."),
+    tags$li(tags$a(href="https://mcmw.abilitynet.org.uk/", "AbilityNet (external website)"),
+            "has advice on making your device easier to use if you have a disability"),
+    br(),
+    p(strong("Compliance status")),
+    br(),
+    p("This site has not yet been evaluated against Web Content Accessibility Guidelines version 2.1
+      level AA standard."),
+    br(),
+    p(strong("Reporting any accessibility problems with this website")),
+    br(),
+    p("If you with to contact us about any accessibility issues you encounter on this site, please email",
+      tags$a(href="mailto:phs.qualityindicators@phs.scot", "phs.qualityindicators@phs.scot")),
+    br(),
+    p(strong("Enforcement procedure")),
+    br(),
+    p("The Equality and Human Rights Commission (EHRC) is responsible for enforcing the Public Sector
+      Bodies (Websites and Mobile Applications)(No.2) Accessibility Regulations 2018 (the 'accessibility
+      regulations'). If you are not happy with how we respond to your complaint,",
+      tags$a(href="https://www.equalityadvisoryservice.com/",
+             "contact the Equality Advisory and Support Service (EASS) (external website).")),
+    br(),
+    p(strong("Preparation of this accessibility statement")),
+    p("This statement was prepared on 24 August 2022. It was last reviewed on 24 August 2022.")
 
   #  bsButton("jump_to_specialties", label = "Go to specialties")
   )
